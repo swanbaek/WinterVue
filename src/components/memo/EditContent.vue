f5ee61706d7ae30777cd8ad4b5ccef09df78bf11
swan <swanbaek@naver.com>
2019-12-04 16:06:28 +0900

    한줄 메모장 CRUD작업 완료
    localStorage사용하여 메모 내용 저장

---
+++ src/components/memo/EditContent.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="col-md-3 col-sm-12 col-xs-12 p-3" >
+    <div class="card" >
+        <img src="images/memo2.png"  class="card-img-top img img-thumbnail" style="height:350px" />
+        <div class="card-img-overlay text-center">
+			<form action="memoEdit.jsp" method="post" class="mt-2">
+				<input type="hidden" name="idx"><p></p>
+				<input type="text" name="title" placeholder="제목"  class="form-control" >
+				<p></p>
+				<textarea name="msg" placeholder="메모내용" class="form-control mt-2"></textarea>
+				<span class="badge badge-success">작성일: a	</span><p></p>
+				<button type="submit" class="btn btn-outline-info">글수정</button>
+			</form> 
+		</div>
+    </div> 
+</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/memo/ViewContent.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="col-md-3 col-sm-12 col-xs-12 p-3" >
+    <div class="card" >
+        <img src="images/memo02.PNG"  class="card-img-top img img-thumbnail" style="height:350px" />
+        <div class="card-img-overlay text-center">
+			<form action="memoEdit.jsp" method="post" class="mt-2"  v-on:submit.prevent="editProcess">
+				<input type="hidden" name="no" :value="editMemo.no"><p></p>
+				<input type="hidden" name="wdate" :value="editMemo.wdate"><p></p>
+				<input type="text" name="title" placeholder="제목"  class="form-control" :value="editMemo.title">
+				<p></p>
+				<textarea name="msg" placeholder="메모내용" class="form-control mt-2"  :value="editMemo.msg"></textarea>
+				<span class="badge badge-success">작성일: {{editMemo.wdate}}	</span><p></p>
+				<button type="submit" class="btn btn-outline-info">글수정</button>
+			</form> 
+		</div>
+    </div> 
+</div>
+</template>
+
+<script>
+    export default {
+		props:['editMemo'],
+		data(){
+			return {
+				memo:{
+					no:0,
+					title:'',
+					msg:'',
+					wdate:''
+				}
+			}
+		},
+		
+		methods:{
+			editProcess:function(e){
+				var tmpMemo={
+					no:e.target.no.value,
+					title:e.target.title.value,
+					msg:e.target.msg.value,
+					wdate:e.target.wdate.value
+				};
+				
+				this.$emit('editEnd',tmpMemo);	
+			}
+		}
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
