c3902f2d7cfe02ba4631443575861140622f62b5
swan <swanbaek@naver.com>
2019-12-04 17:20:33 +0900

    LogOut.vue
    로그아웃 처리 뷰 추가

---
+++ src/components/user/LogOut.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        created(){
+            sessionStorage.removeItem('vue-user');
+            sessionStorage.setItem("isLogin",false);
+            this.$emit('login-result', null);
+            this.$router.push('/');
+        }
+    } 
+</script>
+
+<style lang="scss" scoped>
+
+</style> 
