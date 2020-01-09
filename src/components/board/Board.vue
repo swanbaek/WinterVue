6e73860542877fca15d0c3d13bf86f6591c3a6c0
swan <swanbaek@naver.com>
2020-01-09 18:00:37 +0900

    Board.vue와 Card.vue추가

---
+++ src/components/board/Board.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-info" v-if="loading">Loading Board...</div>
+            <div v-else  class="col-md-12">
+                <h1 class="text-primary">Board</h1>
+                <h2>bno: {{bno}} </h2>
+                <router-link :to="`/board/${bno}/card/1`">Card1</router-link>
+                <router-link :to="`/board/${bno}/card/2`">Card2</router-link>
+                <hr>
+                <router-view></router-view>
+            </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                bno:0,
+                loading:false
+            }
+        },
+        watch:{
+            '$route'(){
+                this.bno=this.$route.params.bno
+               // this.getBoardData();
+            }
+        },
+        created(){
+            
+            // this.bno=this.$route.params.bno;
+            // if(!this.bno){
+            //     this.bno=1;
+            // }
+            this.getBoardData();
+        },
+        methods:{
+            getBoardData(){
+                this.loading=true;
+                setTimeout(()=>{
+                    this.bno=this.$route.params.bno;
+                    this.loading=false;
+                },500)
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/board/Card.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1 class="text-success">Card</h1>
+        <div class="text-danger" v-if="loading">Loading Card...</div>
+        <div v-else>
+            <h2>CNO: {{cno}}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return{
+                cno:0,
+                loading:false
+            }
+        },
+       /*  //[1]
+       watch:{ //중첩라우팅 7min
+           '$route'(){
+               //this.cno=this.$route.params.cno
+               this.getCardData();
+           } 
+        },*/
+        //[2] //중첩라우팅 8:30min
+        watch:{
+            $route:{
+                handler:'getCardData',
+                immediate:true
+            }
+        },
+        created(){
+           // this.cno=this.$route.params.cno;
+          // this.getCardData();
+        },
+        methods:{
+            getCardData(){
+                this.loading=true;
+                setTimeout(()=>{
+                    this.cno=this.$route.params.cno;
+                    this.loading=false;
+                },500)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
