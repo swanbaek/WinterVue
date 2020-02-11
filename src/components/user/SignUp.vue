1a075d94ddb012740d80c9675c935d65c34a8d77
swan <swanbaek@naver.com>
2020-02-11 17:17:19 +0900

    회원 가입 처리 완료

--- src/components/user/SignUp.vue
+++ src/components/user/SignUp.vue
@@ -2,13 +2,32 @@
     <div>
         <div id="join" class="container p-3">
             <h1>SingUp</h1>
-            <form name="frm" action="join.jsp">
+            <!--  회원가입 진행중 표시 -->
+            <template v-if='isProcess == true'>
+                <div  class="alert alert-success">
+                    <h3 class="text-primary">처리 중...</h3>    
+                </div>
+			</template>
+            <template v-else>
+            <div v-if="isSignupFail"  class="alert alert-danger">
+						<h3>회원가입 실패</h3>
+						<p>아이디 중복 여부를 확인해주세요</p>
+			</div>
+
+            <form v-else name="frm" action="join.jsp" @submit.prevent="submit">
         <div class="row">    
             <div class="col-md-6 offset-md-3">
 			
-            Nick Name: <input type="text" name="userNick"
-             placeholder="Nick Name" class="form-control" 
-             v-model="user.name"
+            Name: <input type="text" name="userName"
+             placeholder="User Name" class="form-control" 
+             v-model="user.name">                
+            </div>
+        </div> 
+        <div class="row">    
+            <div class="col-md-6 offset-md-3">   
+            User ID: <input type="text" name="userId"
+             placeholder="User ID" class="form-control" 
+             v-model="user.id"
              v-on:keyup="checkNick">    
             <div v-if="nickErr" class="text-danger">닉네임은 4자이상 6자 이하 입니다.</div>
             </div>
@@ -36,7 +55,7 @@
             </div> 
         <div class="row  m-2">
                 <div class="col-md-6 offset-md-3 p-1">
-                    <button class="btn btn-block btn-outline-success" @click="submit">Signup</buttoN>
+                    <button class="btn btn-block btn-outline-success">Signup</buttoN>
                 </div>
         </div>
 
@@ -47,6 +66,8 @@
 			
         </div>
         </form>
+            </template> 
+            <!-- isProcess==false -->
     </div>    
     </div>
 </template>
@@ -58,13 +79,15 @@ import axios from 'axios';
              return{
                 user:{
                     name:'',
-                    id:'test',
+                    id:'',
                     pwd:'',
                     email:''
                 },
                 nickErr:false,
                 pwdErr:false,
                 pwdErr2:false,
+                isProcess:false,
+                isSignupFail:false
             }
          },
             methods:{
@@ -96,8 +119,14 @@ import axios from 'axios';
                    if(!this.nameErr && !this.pwdErr && !this.pwdErr2){
                       // localStorage.setItem('vue-user',JSON.stringify(this.user));
                      //alert('회원 가입 요청 중...');
-                     this.requestJoin();
-                     this.$router.push('/login')
+                     
+                         this.isProcess=true;
+                     setTimeout(()=>{
+                         this.requestJoin();
+                         this.isProcess=false;
+                     },3000)
+                     
+                     //this.$router.push('/login')
                 }else{
                     alert('입력값에 오류가 있습니다.');
                 }
@@ -105,19 +134,29 @@ import axios from 'axios';
                 ,requestJoin(){
                     let url="http://localhost:9090/VueBackend/join.jsp"
                     let params=new URLSearchParams();    
-                    alert(this.user.name);                
+                    
                     params.append('name',this.user.name);
                     params.append('id',this.user.id);
                     params.append('pwd',this.user.pwd);
                     params.append('email',this.user.email);
-
+                    var vm=this;
                     axios.post(url, params)
                         .then(function(response){
-                            alert(response);
+                          //  alert(response.data.result);
+                           let resultData=parseInt(response.data.result);
+                            if(resultData>0){
+                                //vm.isProcess=false;
+                                vm.isSignupFail=false;
+                                alert('회원가입 처리 성공...로그인을 하세요.')
+                                vm.$router.push('/')
+                            }else{
+                                vm.isSignupFail=true;
+                            }
                         })
                         .catch(function(err){
 
                             alert(err.message);
+                            vm.isSignupFail=true;
                         })
                 }
             }//methods
